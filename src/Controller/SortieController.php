ad2444c026450207c33c03339dc4cd191591bc2f
HishamTAHA90 <hisham.taha2021@campus-eni.fr>
2022-01-26 11:16:46 +0100

    create formoulaire2

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -14,7 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form;
-
+use App\Form\ListSortieType;
 
 
 class SortieController extends AbstractController
@@ -22,7 +22,7 @@ class SortieController extends AbstractController
     /**
      * @Route("/sortie", name="sortie_create")
      */
-    public function create(Request $request, EntityManagerInterface $entityManager ,ParticipantRepository $participantRepository): Response
+    public function create(Request $request, EntityManagerInterface $entityManager, ParticipantRepository $participantRepository): Response
 
     {
         $sortie = new Sortie();
@@ -32,33 +32,52 @@ class SortieController extends AbstractController
         if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
 
 
-            if($sortieForm->get('save')->isClicked()){
+            if ($sortieForm->get('save')->isClicked()) {
 
 
-               $etat = $entityManager->getRepository(Etat::class)->findOneBy(['libelle'=>'Créée']);
-               $this->addFlash('success',"la sortie a été créée"); }
-            else{
-                $etat=$entityManager->getRepository(Etat::class)->findOneBy(['libelle'=>'Ouverte']);
-                $this->addFlash('success',"la sortie a été publié");
+                $etat = $entityManager->getRepository(Etat::class)->findOneBy(['libelle' => 'Créée']);
+                $this->addFlash('success', "la sortie a été créée");
+            } else {
+                $etat = $entityManager->getRepository(Etat::class)->findOneBy(['libelle' => 'Ouverte']);
+                $this->addFlash('success', "la sortie a été publié");
             }
-          //  $currentUserUsername = $this->getUser()->getUserIdentifier();
 
-            //$sortie->setOrganisateur($currentUserUsername);
-         $organisateur = $participantRepository->find($this->getUser()->getUserIdentifier());
+            $organisateur = $participantRepository->find($this->getUser());
 
             $sortie->setEtat($etat);
-           $sortie->setOrganisateur($organisateur);
-
+            $sortie->setOrganisateur($organisateur);
 
 
             $entityManager->persist($sortie);
             $entityManager->flush();
 
-            return $this->redirectToRoute('affichage.html.twig');
+            return $this->redirectToRoute('sortir_accueil');
         }
 
 
         return $this->render('sortie/create.html.twig', [
+            'sortieForm' => $sortieForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/accueil", name="sortir_accueil")
+     */
+    public function index(Request $request, SortieRepository $sortieRepository): Response
+    {
+        $sorties = $sortieRepository->findAll();
+
+        $sortieForm = $this->createForm(ListSortieType::class);
+        $sortieForm->handleRequest($request);
+
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $sortieForm->getData();
+            //var_dump($data);
+        }
+
+        return $this->render('sortie/accueil.html.twig', [
+            'sorties' => $sorties,
             'sortieForm' => $sortieForm->createView()
         ]);
     }
