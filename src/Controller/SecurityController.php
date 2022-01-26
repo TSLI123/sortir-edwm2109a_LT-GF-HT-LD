d8f6a50e15321f0a1143d75a46bb59a1a0561065
gfrerejouan@gmail.com <Annee#2018#>
2022-01-26 10:39:05 +0100

    modif route accueil sortie #2

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,16 +14,16 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-         #if ($this->getUser()) {
-        #   return $this->redirectToRoute('sortir_accueil');
-      #  }
+        # if ($this->getUser()) {
+         # return $this->redirectToRoute('sortir_accueil');
+      # }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('sortie/accueil.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
     /**
      * @Route("/logout", name="app_logout")
