d8ce54538729e27c0652f82631e4854ae8f52bdd
TSLI123 <thomas.leroi2021@campus-eni.fr>
2022-01-26 11:38:53 +0100

    - Manage filters

--- src/Form/ListSortieType.php
+++ src/Form/ListSortieType.php
@@ -9,41 +9,53 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\Validator\Util\PropertyPath;
 
 class ListSortieType extends AbstractType
 {
+
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
         $builder
             ->add('campus', EntityType::class, [
                 'class' => Campus::class,
                 'choice_label' => 'nom',
                 'multiple' => false
             ])
-
             ->add('search', SearchType::class, [
-                'required' => false
+                'required' => false,
+                'constraints' => [
+                    new Length(['max' => 50]),
+                ],
             ])
-
-            ->add('entre', DateType::class, [
+            ->add('from', DateType::class, [
+                'label' => 'entre',
                 'html5' => true,
                 'widget' => 'single_text'
             ])
-            ->add('et', DateType::class, [
+            ->add('to', DateType::class, [
+                'label' => 'et',
                 'html5' => true,
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'constraints' => [
+                    //new GreaterThan( $propertyAccessor->getValue($builder,'child.from'))
+                ]
             ])
             ->add('choix', ChoiceType::class, [
                 'choices' => [
-                    "Sorties dont je suis l'organisateur/trice" => 'org',
-                    'Sorties auquelles je suis inscrit/e' => 'ins',
-                    'Sorties auquelles je ne suis pas inscrit/e' => 'pasIns',
-                    'Sorties passées' => 'passe'
+                    "Sorties dont je suis l'organisateur/trice" => 'organisateur',
+                    'Sorties auquelles je suis inscrit/e' => 'inscrit',
+                    'Sorties auquelles je ne suis pas inscrit/e' => '/inscrit',
+                    'Sorties passées' => 'past'
                 ],
                 'multiple' => true,
                 'expanded' => true
-            ])
-        ;
+            ]);
     }
 
 }
