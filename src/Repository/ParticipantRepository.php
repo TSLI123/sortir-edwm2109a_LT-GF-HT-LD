a0cbd837ad9b0f114e9f4f474c2c84855055e07f
gfrerejouan@gmail.com <Annee#2018#>
2022-01-26 12:29:30 +0100

    authentification avec pseudo et mail

--- src/Repository/ParticipantRepository.php
+++ src/Repository/ParticipantRepository.php
@@ -4,10 +4,13 @@ namespace App\Repository;
 
 use App\Entity\Participant;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Select;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @method Participant|null find($id, $lockMode = null, $lockVersion = null)
@@ -15,7 +18,7 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  * @method Participant[]    findAll()
  * @method Participant[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ParticipantRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
+class ParticipantRepository extends ServiceEntityRepository implements PasswordUpgraderInterface, UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
@@ -34,6 +37,21 @@ class ParticipantRepository extends ServiceEntityRepository implements PasswordU
         $user->setMotPasse($newHashedPassword);
         $this->_em->persist($user);
         $this->_em->flush();
+    }
+
+    public function loadUserByIdentifier(string $pseudoOrEmail): ?Participant
+    {
+        $entityManager = $this->getEntityManager();
+
+        return $entityManager->createQuery(
+            'SELECT p
+            FROM App\Entity\Participant p
+            WHERE p.pseudo = :query
+            OR p.email= :query
+            '
+        )
+            ->setParameter('query', $pseudoOrEmail)
+            ->getOneOrNullResult();
     }
 
     // /**
@@ -64,4 +82,13 @@ class ParticipantRepository extends ServiceEntityRepository implements PasswordU
         ;
     }
     */
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement @method null loadUserByIdentifier(string $identifier)
+    }
+
+    public function loadUserByUsername(string $username)
+    {
+        // TODO: Implement loadUserByUsername() method.
+    }
 }
