20c4124a65ca7c5f978e37d7c17f05c7bc91056f
TSLI123 <thomas.leroi2021@campus-eni.fr>
2022-02-03 16:47:15 +0100

    - add listener pour recuperer le lieu avant soumission

--- src/Form/CreateSortieType.php
+++ src/Form/CreateSortieType.php
@@ -17,6 +17,9 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CreateSortieType extends AbstractType
@@ -68,22 +71,46 @@ class CreateSortieType extends AbstractType
                 'choice_label' => 'nom',
                 'placeholder' => 'saisir une ville'
             ])
-            ->add('lieu', EntityType::class, [
-                'class' => Lieu::class,
-                'choice_label' => 'nom',
-                //'choices'=>[]
-            ])
-
             ->add('save', SubmitType::class, [
                 'label' => 'Enregistrer',
             ])
             ->add('publier', SubmitType::class, [
                 'label' => 'Publier',
             ])
-            ->add('reset', ResetType::class,[
-                'label'=>'Annuler'
+            ->add('reset', ResetType::class, [
+                'label' => 'Annuler'
             ]);
 
+        $formModifier = function (FormInterface $form, Ville $ville = null) {
+            $lieux = null === $ville ? [] : $ville->getLieux();
+
+            $form->add('lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'placeholder' => '',
+                'choice_label' => 'nom',
+                'choices' => $lieux,
+            ]);
+        };
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) use ($formModifier) {
+                $formModifier($event->getForm(), null);
+            }
+        );
+
+        $builder->get('ville')->addEventListener(
+            FormEvents::POST_SUBMIT,
+            function (FormEvent $event) use ($formModifier) {
+                // It's important here to fetch $event->getForm()->getData(), as
+                // $event->getData() will get you the client data (that is, the ID)
+                $ville = $event->getForm()->getData();
+
+                // since we've added the listener to the child, we'll have to pass on
+                // the parent to the callback functions!
+                $formModifier($event->getForm()->getParent(), $ville);
+            }
+        );
 
 
     }
