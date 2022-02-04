7a9a0e21db8010a73870a6fa9f8dbadb2e995578
TSLI123 <thomas.leroi2021@campus-eni.fr>
2022-02-04 10:55:03 +0100

    - update message annulation non autorisée

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -172,9 +172,8 @@ class SortieController extends AbstractController
             }
 
 
-        }
-        else{
-            $this->addFlash('faileur', ('Sortie "' . $sortie->getNom() . '" ne peut pas être annulé !'));
+        } else{
+            $this->addFlash('failure', ('Action non autorisée !' ));
             return $this->redirectToRoute('sortie_accueil');
         }
         return $this->render('sortie/annuler.html.twig', [
